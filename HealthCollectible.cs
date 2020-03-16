b448de374527474a10be8674cec0ba7ec86a7f0b
Sparrow97 <vanya1228@yandex.ru>
2020-03-16 20:02:16 +0300

    Create HealthCollectible.cs

---
+++ HealthCollectible.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("В наш коллайдер зашел : " + other);
+
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            if (controller.Health < controller.maxHealth)
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+}
